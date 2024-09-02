a0b12affcc2047d92e4bdb671e4c1c19fb3bc4df
Eric Mong <m.changhsi@gmail.com>
2024-09-02 23:47:09 +0800

    Remove logs

--- client/src/utils/date/index.tsx
+++ client/src/utils/date/index.tsx
@@ -1,7 +1,5 @@
 export const getDaysLeft = (timestamp: number) => {
   const currentDate = new Date();
-  console.log(+currentDate);
-  console.log(+timestamp);
 
   if (+currentDate >= timestamp * 1_000) return 0;
   const remainingSeconds = timestamp * 1_000 - +currentDate;
