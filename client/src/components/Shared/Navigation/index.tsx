70d89d54a6f1a99f0c1a733b6da8191354be7175
Eric Mong <m.changhsi@gmail.com>
2024-09-03 22:38:07 +0800

    Add landing redirection

--- client/src/components/Shared/Navigation/index.tsx
+++ client/src/components/Shared/Navigation/index.tsx
@@ -6,11 +6,14 @@ import {
 } from "./style";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import ThemeToggle from "../Theme/ThemeToggle";
+import Link from "next/link";
 
 const Navigation = forwardRef((_, ref: LegacyRef<HTMLElement>) => {
   return (
     <NavigationContainer ref={ref}>
-      <NavigationIcon>Crowdfunding</NavigationIcon>
+      <Link href="/">
+        <NavigationIcon>Crowdfunding</NavigationIcon>
+      </Link>
       <NavigationSection>
         <ThemeToggle />
         <ConnectButton />
